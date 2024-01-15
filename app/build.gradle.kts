60800abf941d61040924c0d8160a81ca7ee06e75
Constis2412 <pfe20424@spengergasse.at>
2024-01-15 09:06:58 +0100

    some

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "at.spengergasse.helpateer"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "at.spengergasse.helpateer"
